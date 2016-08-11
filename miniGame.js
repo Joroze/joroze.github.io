249fb789ea426b0432a9acbb805e08310290959a
Jordan <Joroze@users.noreply.github.com>
2016-08-10 23:50:58 -0400

    Update miniGame.js

--- miniGame.js
+++ miniGame.js
@@ -1,6 +1,6 @@
 
 
-var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update, render: render });
+var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game-area', { preload: preload, create: create, update: update, render: render });
 
 function preload() {
 
