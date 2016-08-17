b884f9c1ce9ec450c5bc23040ea32ce9f13619b2
Jordan <Joroze@users.noreply.github.com>
2016-08-17 09:49:27 -0400

    Update typed.js

--- typed.js
+++ typed.js
@@ -436,3 +436,38 @@
 
 
 }(window.jQuery);
+
+setTimeout(function(){
+        $(".element0").typed({
+            strings: ["- open to new ideas and eager to learn"],
+            typeSpeed: 30, // typing speed
+            backDelay: 750, // pause before backspacing
+            loop: false, // loop on or off (true or false)
+            loopCount: false, // number of loops, false = infinite
+            callback: function(){ } // call function after typing is done
+        })(jQuery);
+    }, 0);
+
+setTimeout(function(){
+        $(".element1").css("display", "inherit");
+        $(".element1").typed({
+            strings: ["- able to think outside the box"],
+            typeSpeed: 30, // typing speed
+            backDelay: 750, // pause before backspacing
+            loop: false, // loop on or off (true or false)
+            loopCount: false, // number of loops, false = infinite
+            callback: function(){ } // call function after typing is done
+        })(jQuery);
+    }, 2000);
+
+setTimeout(function(){
+        $(".element2").css("display", "inherit");
+        $(".element2").typed({
+            strings: ["- organized and to the point"],
+            typeSpeed: 30, // typing speed
+            backDelay: 750, // pause before backspacing
+            loop: false, // loop on or off (true or false)
+            loopCount: false, // number of loops, false = infinite
+            callback: function(){ } // call function after typing is done
+        })(jQuery);
+    }, 4000);
