0232370dc6e67bc1354da4287d307994e77bf5d9
Jordan <Joroze@users.noreply.github.com>
2016-08-17 18:06:58 -0400

    Update main.js

--- main.js
+++ main.js
@@ -28,9 +28,9 @@ setTimeout(function(){
             loopCount: false, // number of loops, false = infinite
             callback: function(){ } // call function after typing is done
         });
-    }, 5000);
+    }, 4000);
 
-setTimeout(function(){
+(function(){
         $(".element2").typed({
             strings: ["- creative"],
             typeSpeed: 40, // typing speed
