0d76103bd8fb0404c0274b245a9c90e2f46e4e4c
Jordan <Joroze@users.noreply.github.com>
2016-08-17 11:03:33 -0400

    Update main.js

--- main.js
+++ main.js
@@ -20,7 +20,6 @@ setTimeout(function(){
     }, 0);
 
 setTimeout(function(){
-        $(".element1").css("display", "inherit");
         $(".element1").typed({
             strings: ["- able to think outside the box"],
             typeSpeed: 30, // typing speed
@@ -32,7 +31,6 @@ setTimeout(function(){
     }, 2000);
 
 setTimeout(function(){
-        $(".element2").css("display", "inherit");
         $(".element2").typed({
             strings: ["- organized and to the point"],
             typeSpeed: 30, // typing speed
