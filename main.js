a573250a0a59530cce438d32d4ad6fa28ac79955
Jordan <Joroze@users.noreply.github.com>
2016-08-17 11:57:17 -0400

    Update main.js

--- main.js
+++ main.js
@@ -26,7 +26,7 @@ setTimeout(function(){
             backDelay: 750, // pause before backspacing
             loop: false, // loop on or off (true or false)
             loopCount: false, // number of loops, false = infinite
-            callback: function(){ } // call function after typing is done
+            callback: function(){shift(); } // call function after typing is done
         });
     }, 5000);
 
